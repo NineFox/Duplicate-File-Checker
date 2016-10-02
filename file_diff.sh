7d2ac19d3dda0bc0b930378c4bde3826f77781cd
Komatsubara <express.yukiguni.583@gmail.com>
2016-10-02 20:10:15 +0900

    重複しているファイルと重複していないファイルが出力できないバグを修正

--- file_diff.sh
+++ file_diff.sh
@@ -1,4 +1,7 @@
-#!/bin/sh
+#!/bin/bash
+
+sourceArray=()
+destinationArray=()
 
 #あるフォルダ以下にあるファイルを全て出力する関数
 function recurDirectry() {
@@ -8,7 +11,7 @@ function recurDirectry() {
 
     for filePath in $files; do
         if [ -f $filePath ]; then
-            fileArray+=("$filePath")
+            fileArray+=($filePath)
             echo $filePath
         elif [ -d $filePath ]; then
             recurDirectry "${filePath}/*" "${fileArray}" "${directryArray}"
@@ -16,55 +19,76 @@ function recurDirectry() {
     done
 }
 
+#md5sum コマンドからファイルパスを削除する
+function getMd5SumOnly() {
+    local md5sumResult=`md5sum $1`
+    echo "${md5sumResult%% *}"
+}
+
 #2つの配列から重複している要素だけを取り出す
 function duplicateElements() {
-    local sourceArray=$1
-    local destinationArray=$2
+
     local resultArray=()
 
-    for p in $sourceArray; do
-        for q in $destinatinationArray; do
-            if [ md5sum $p = md5sum $q ]; then
-                resultArray+=$p
+    for p in ${sourceArray[@]}; do
+        for q in ${destinatinationArray[@]}; do
+            #echo "this is q = ${q}"
+            #echo "this is p = ${p}"
+            if [ `getMd5SumOnly $p` = `getMd5SumOnly $q` ]; then
+                resultArray+=($p)
             fi
         done
     done
-    echo $resultArray
+    echo ${resultArray[@]}
 }
 
 #2つの配列から重複していない要素だけを取り出す
 function noDuplicateElements() {
-    local sourceArray=$1
-    local destinationArray=$2
+
     local resultArray=()
     local isContain=0
 
-    for p in $sourceArray; do
+    for p in ${sourceArray[@]}; do
         isContain=0
-        for q in $destinatinationArray; do
-            if [ md5sum $p -eq md5sum $q ]; then
+        for q in ${destinatinationArray[@]}; do
+            if [ `getMd5SumOnly $p` = `getMd5SumOnly $q` ]; then
                 isContain=1
                 break;
             fi
         done
-        if [ 0 -eq $isContain ]; then
-            resultArray+=$p
+
+        if [ "0" -eq "$isContain" ]; then
+            resultArray+=($p)
         fi
     done
-    echo $resultArray
+    echo ${resultArray[@]}
 }
 
 
-#比較元
-comparisonSource=`recurDirectry "${1}*" "" ""`
-#比較先
-comparisonDestination=`recurDirectry "${2}*" "" ""`
 
+#比較元
+sourceArray+=(`recurDirectry "${1}*" "" ""`)
+#比較先
+destinatinationArray+=(`recurDirectry "${2}*" "" ""`)
+
+echo ${sourceArray[@]}
+
+echo -e \
+
+echo ${destinatinationArray[@]}
+
+echo -e \
 
 echo "Duplicate files"
-echo `duplicateElements $comparisonSource $comparisonDestination`
+resultDuplicate+=(`duplicateElements`)
+for i in ${resultDuplicate[@]}; do
+    echo $i
+done
 
 echo -e \
 
 echo "No duplicate files"
-echo `noDuplicateElements $comparisonSource $comparisonDestination`
+resultNoDuplicate+=(`noDuplicateElements`)
+for i in ${resultNoDuplicate[@]}; do
+    echo $i
+done
